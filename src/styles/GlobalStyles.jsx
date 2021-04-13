be2f400e08367b24fdc53188e413872eb03f7d0b
MaysharaF <mayshara@alunos.utfpr.edu.br>
2021-04-13 20:10:22 -0300

    Adicionando estilização global

---
+++ src/styles/GlobalStyles.jsx
@@ -0,0 +1,29 @@
+import { createGlobalStyle } from "styled-components";
+
+export default createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    outline: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    background: rgb(18, 18, 20) ;
+    -webkit-font-smoothing: antialiased;
+  }
+
+  body, #root {
+    max-height: 100vh;
+    max-width: 100vw;
+
+    width: 100vw;
+    height: 100vh;
+    font: 16px Roboto, sans-serif; 
+  }
+
+  button {
+    cursor: pointer;
+  }
+
+`;
---
+++ src/styles/Icons.jsx
@@ -0,0 +1,3 @@
+export {Email} from '@styled-icons/material/Email';
+
+export {LockAlt} from '@styled-icons/boxicons-solid/LockAlt';
