28db6fc099875ef79e478ef3e11c3dcbbf5f1432
MaysharaF <mayshara@alunos.utfpr.edu.br>
2021-05-24 23:18:05 -0300

    Corrigindo layout da tela de signup

--- src/pages/Signup/styles.js
+++ src/pages/Signup/styles.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   width: 100%;
-  height: 70%;
+  height: 100%;
   display: flex;
   align-items: center;
   justify-content: center;
